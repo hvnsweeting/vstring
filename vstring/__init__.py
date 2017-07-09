de06888a1c0397e39f93dedf50c047dc7e899e04
Hung Nguyen Viet <hvn@familug.org>
2017-07-09 16:55:18 +0700

    Assert is great

--- vstring/__init__.py
+++ vstring/__init__.py
@@ -1,8 +1,17 @@
-# coding: utf-8
+import sys
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    string_types = str
+else:
+    string_types = basestring  # NOQA
 
 
 def remove_tone(s):
     '''Removes all tones from string `s`'''
+    assert isinstance(s, string_types)
     pass
 
 
@@ -12,9 +21,11 @@ def to_ascii(s):
     This is the ASCII version which causes ambiguous when phones don't support
     Vietnamese.
     '''
+    assert isinstance(s, string_types)
     pass
 
 
 def get_tone(word):
     '''Get tone from given word'''
+    assert isinstance(word, string_types)
     pass
